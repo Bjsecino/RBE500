1f5dd9e8ff7aec50d2f59997a2025dde38465d02
Pratik Jawahar <55033105+PRAkTIKal24@users.noreply.github.com>
2019-12-03 10:22:25 -0500

    Add files via upload

--- scripts/pub2.py
+++ scripts/pub2.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 
-from rbe_500.srv import pose,poseResponse
 from std_msgs.msg import Float64
 from sensor_msgs.msg import JointState
 import rospy
 import time
-
-pub = rospy.Publisher('/custom_scara/joint3_position_controller/command', Float64, queue_size=10)
 
 def callback_jointstates(msg):
 	global pub
@@ -15,7 +12,6 @@ def callback_jointstates(msg):
 	print("Q3 from gazebo: " + str("%2.3f" %
                            Q3))
 	
-
 
 if __name__ == "__main__":
 	rospy.init_node('testNode')

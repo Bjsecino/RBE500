478c674bf73b234bee135a2b0e36abc26972310f
Madhan-Suresh <madhanksb1234@gmail.com>
2019-12-05 14:40:43 -0500

    joint1 joint2 control

--- scripts/reference_pos_server.py
+++ scripts/reference_pos_server.py
@@ -4,8 +4,10 @@ from rbe_500.srv import position_controller_reference,position_controller_refere
 from std_msgs.msg import Float64
 
 def handle_set_ref(data):
-	rospy.loginfo(data.ref)
-	pub.publish(data.ref)
+	rospy.loginfo(data)
+	pub1.publish(data.ref1)
+	pub2.publish(data.ref2)
+	pub3.publish(data.ref3)
 	ret.resp = 1
 	return ret
 
@@ -17,7 +19,9 @@ def position_control_server():
 
 if __name__=="__main__" : 
 	rospy.init_node('reference_pos_server',anonymous = True)
-	pub = rospy.Publisher('/custom_scara/joint3_position_controller/command',Float64,queue_size=1)
+	pub1 = rospy.Publisher('/custom_scara/joint1_position_controller/command',Float64,queue_size=1)
+	pub2 = rospy.Publisher('/custom_scara/joint2_position_controller/command',Float64,queue_size=1)
+	pub3 = rospy.Publisher('/custom_scara/joint3_position_controller/command',Float64,queue_size=1)
 	ret = position_controller_referenceResponse()
 	position_control_server()
 
